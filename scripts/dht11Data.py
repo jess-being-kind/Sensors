cab23c6c99a1930adf995111c272f10eab984d56
jess-being-kind <schlumpsj@gmail.com>
2026-06-18 18:45:49 -0700

    Logging changes for adding manual sample frequency to mpu6050. Note dht11 only supports ~2Hz max so it is not settable

--- scripts/dht11Data.py
+++ scripts/dht11Data.py
@@ -6,10 +6,12 @@ import sys
 from machine import Pin, time_pulse_us
 from time import sleep_ms, sleep_us
 
-## Check whether argument(s) were provided
-if len(sys.argv) > 1:
-    sampleRate = sys.argv[1]
-    debug = sys.argv[2]
+try:
+    from run_config import samplesRecorded as dataPoints, frequencySamples, collectData
+except ImportError:
+    samplesRecorded = 100
+    frequencySamples = 2
+    collectData = True
 
 ## Initialize Variables
 # Define signal lists
@@ -17,86 +19,87 @@ bitDecode = []
 bytesOut = []
 
 # Define boolean config settings
-debug = False
-calibrateThreshold = True
+if collectData:
+    print("dht11Data.py executing: YES")
+    sleep_ms(1000) 
+    debug = False
+    calibrateThreshold = True
 
-# Initialize hardware pin GC20
-dataPin = Pin(20)
+    # Initialize hardware pin GC20
+    dataPin = Pin(20)
 
-# Define integer config settings
-handshakeLimit = 5
-pulseThreshold = 25
-dataPoints = 50
-baroOffset = 13        # Measured delta w/ 2nd "true" sensor @ 50% RH
-tempOffset = -0.7
-samplePeriod = 500     # Sample period in [ms]
+    # Define integer config settings
+    handshakeLimit = 5
+    pulseThreshold = 25
+    baroOffset = -13        # Measured delta w/ 2nd "true" sensor @ 50% RH
+    tempOffset = -0.7
+    period_ms = int(1000 / frequencySamples)
 
-#if sys.argv[1]:
-#    samplePeriod = 1 // sampleRate     # Sample rate in [Hz] == 1/500ms # type:ignore
+    def dht11Start(handshakeFailed):
 
-def dht11Start(handshakeFailed):
+        readFail, readSuccess = ((0, 0))
+        lowHeader = [0] * 40
+        highPulse = [0] * 40
 
-    readFail, readSuccess = ((0, 0))
-    lowHeader = [0] * 40
-    highPulse = [0] * 40
-
-    dataPin.init(Pin.OUT, Pin.PULL_UP)
-    gc.collect()
-    gc.disable()   
-    dataPin.value(0)
-    sleep_ms(18)
-    dataPin.value(1)
-    sleep_us(20)
-    dataPin.init(Pin.IN,Pin.PULL_UP)
-    handshakeResponseA = time_pulse_us(dataPin, 0, 100)
-    handshakeResponseB = time_pulse_us(dataPin, 1, 100)    
-    
-    if handshakeResponseA == -1 or handshakeResponseB == -1 :
-        print("Handshake failed ", handshakeFailed + 1, "times, retrying ", handshakeLimit, "times")
-    
-        if handshakeFailed < 5:# and handshakeResponseA is None or handshakeResponseB is None:
-            handshakeFailed += 1
-            sleep_ms(1000)
-            dht11Start(handshakeFailed)
-        else:
-            raise AssertionError("Failed DHT11 handshake ", handshakeFailed, "times")
-
-    for i in range(40):
-        lowHeader[i] = (time_pulse_us(dataPin, 0, 100))
-        highPulse[i] = (time_pulse_us(dataPin, 1, 100))
-    gc.enable()
-
-    for i in range(40):
-        if lowHeader[i] < 0 or highPulse[i] < 0:
-            readFail += 1
-        elif 1 < lowHeader[i] < 50:
-            if highPulse[i] > pulseThreshold:
-                bitDecode.append(1)
+        dataPin.init(Pin.OUT, Pin.PULL_UP)
+        gc.collect()
+        gc.disable()   
+        dataPin.value(0)
+        sleep_ms(18)
+        dataPin.value(1)
+        sleep_us(20)
+        dataPin.init(Pin.IN,Pin.PULL_UP)
+        handshakeResponseA = time_pulse_us(dataPin, 0, 100)
+        handshakeResponseB = time_pulse_us(dataPin, 1, 100)    
+        
+        if handshakeResponseA == -1 or handshakeResponseB == -1 :
+            print("Handshake failed ", handshakeFailed + 1, "times, retrying ", handshakeLimit, "times")
+        
+            if handshakeFailed < 5:# and handshakeResponseA is None or handshakeResponseB is None:
+                handshakeFailed += 1
+                sleep_ms(1000)
+                dht11Start(handshakeFailed)
             else:
-                bitDecode.append(0)
-            readSuccess += 1
+                raise AssertionError("Failed DHT11 handshake ", handshakeFailed, "times")
 
-    for start in range(0, 40, 8):
-        byte = 0
-        for bit in bitDecode[start:start+8]:
-            byte = (byte << 1) | bit
-        bytesOut.append(byte)
+        for i in range(40):
+            lowHeader[i] = (time_pulse_us(dataPin, 0, 100))
+            highPulse[i] = (time_pulse_us(dataPin, 1, 100))
+        gc.enable()
 
-    thresholdError = readFail / max(readSuccess, 1)
-    if debug == True:
-        print("Read threshold error (%): " , 100*thresholdError, "\n", "Bits read successfully: ", readSuccess, "\n", "Bits failed to read: ", readFail, "\n", sep="")
+        for i in range(40):
+            if lowHeader[i] < 0 or highPulse[i] < 0:
+                readFail += 1
+            elif 1 < lowHeader[i] < 50:
+                if highPulse[i] > pulseThreshold:
+                    bitDecode.append(1)
+                else:
+                    bitDecode.append(0)
+                readSuccess += 1
 
-    return bytesOut
+        for start in range(0, 40, 8):
+            byte = 0
+            for bit in bitDecode[start:start+8]:
+                byte = (byte << 1) | bit
+            bytesOut.append(byte)
 
-for point in range(dataPoints):
-    
-    bytesOut = dht11Start(0)
+        thresholdError = readFail / max(readSuccess, 1)
+        if debug == True:
+            print("Read threshold error (%): " , 100*thresholdError, "\n", "Bits read successfully: ", readSuccess, "\n", "Bits failed to read: ", readFail, "\n", sep="")
 
-    print(
-        f"{bytesOut[0]},",
-        f"{bytesOut[1]},",
-        f"{bytesOut[2]},",
-        f"{bytesOut[3]},",
-        f"{bytesOut[0] + bytesOut[1] + bytesOut[2] + bytesOut[3] == bytesOut[4]}")
+        return bytesOut
 
-    sleep_ms(samplePeriod)
+    for point in range(dataPoints):
+        
+        bytesOut = dht11Start(0)
+
+        print(
+            f"{bytesOut[0] + baroOffset},",
+            f"{bytesOut[1]},",
+            f"{9/5*(bytesOut[2] + tempOffset) + 32},",
+            f"{bytesOut[3]},",
+            f"{bytesOut[0] + bytesOut[1] + bytesOut[2] + bytesOut[3] == bytesOut[4]}")
+
+        sleep_ms(period_ms)
+else:
+    print("dht11Data.py executing: NO")
